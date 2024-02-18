e50ad7715b0d558aee6b27c3fceb35319d6473cd
arya-deshpande <deshpandearya9@gmail.com>
2024-02-18 01:53:02 -0800

    //juniper!!!

---
+++ flutter_application_1/lib/juniper.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/blueORgreen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'welcomePage.dart';
+
+class Juniper extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color.fromARGB(255, 18, 32, 47),
+      ),
+      home: Scaffold(
+        body: ListView(children: [
+          AndroidLarge12(),
+        ]),
+      ),
+    );
+  }
+}
+
+class AndroidLarge12 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+  Container(
+  width: 284,
+  height: 46,
+  padding: const EdgeInsets.all(10),
+  decoration: ShapeDecoration(
+    color: Color(0xFFBC3124),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+  ),
+  child: Row(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      SizedBox(
+        width: 270,
+        child: Text(
+          'Return to Main',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Color(0xFFFFE4C2),
+            fontSize: 25,
+            fontFamily: 'Space Grotesk',
+            fontWeight: FontWeight.w400,
+            height: 0,
+          ),
+        ),
+      ),
+    ],
+  ),
+)
+      ]
+    );
+  }
+}
