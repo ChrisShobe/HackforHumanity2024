ef0418211e9deeccd50ae2e65b4eb7a00df0e6f6
ChrisShobe <chrisshobe2000@gmail.com>
2024-02-18 00:26:06 -0800

    bark firm button and text made, soft not appearing

--- flutter_application_1/lib/bark.dart
+++ flutter_application_1/lib/bark.dart
@@ -1,191 +1,147 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class BarkPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Bark'),
       ),
-      body: Center(
-        child: Container(
-          width: 360,
-          height: 773,
-          clipBehavior: Clip.antiAlias,
-          decoration: BoxDecoration(color: Colors.white),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-             Container(
-  width: 367,
-  height: 800,
-  clipBehavior: Clip.antiAlias,
-  decoration: BoxDecoration(color: Colors.white),
-  child: Stack(
-    children: [
-      Positioned(
-        left: -77,
-        top: -45,
-        child: Container(
-          width: 513,
-          height: 873,
-          decoration: BoxDecoration(color: Color(0xFF639F32)),
-        ),
-      ),
-      Positioned(
-        left: 11,
-        top: 5,
-        child: Container(
-          width: 345,
-          height: 790,
-          decoration: BoxDecoration(color: Color(0xFFFFE4C2)),
-        ),
-      ),
-      Positioned(
-        left: 37,
-        top: 135,
-        child: SizedBox(
-          width: 285,
-          height: 70,
-          child: Text(
-            'Is the bark firm or soft?',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Color(0xFF3391CC),
-              fontSize: 30,
-              fontFamily: 'Space Grotesk',
-              fontWeight: FontWeight.w400,
-              height: 0,
+      body: Stack(
+        children: <Widget>[
+          Container(
+            color: Colors.white
+          ),
+          Positioned(
+            child: Container(
+              width: width, // Use the full available width
+              height: height, // Use the full available height
+              color: Color(0xFF639F32),
             ),
           ),
-        ),
-      ),
-      Positioned(
-        left: 142,
-        top: 593,
-        child: Container(
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(10),
-                decoration: ShapeDecoration(
-                  color: Color(0xFF3391CC),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-                  shadows: [
-                    BoxShadow(
-                      color: Color(0x3F000000),
-                      blurRadius: 4,
-                      offset: Offset(0, 4),
-                      spreadRadius: 0,
-                    )
-                  ],
+          Positioned(
+            left: 10,
+            top: 10,
+            child: Container(
+              width: width - 20, //345,
+              height: height - 75,
+              color: Color(0xFFFFE4C2),
+            ),
+          ),
+          Positioned(
+            left: width * 0.25, // Adjust the left position
+            top: height * 0.05, // Adjust the top position
+            child: SizedBox(
+              width: width * 0.44, // Adjust the width
+              height: height * 0.08, // Adjust the height
+              child: Center(
+                child: Text(
+                  'Is the bark firm or soft?',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.spaceGrotesk(
+                    color: Color(0xFF3391CC),
+                    fontSize: 30,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Soft',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 30,
-                        fontFamily: 'Space Grotesk',
-                        fontWeight: FontWeight.w400,
-                        height: 0,
-                      ),
+              ),
+            ),
+          ),
+          Positioned(
+          left: width * 0.4,
+          top: 200,
+          child: Builder(
+            builder: (context) => ElevatedButton(
+              onPressed: () {
+                //sequoia tree
+              },
+              child: Text('Button'), // Add this line
+              style: ElevatedButton.styleFrom(
+                primary: Color(0xFF3391CC),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                child: Center(
+                  child: Text(
+                    'Soft',
+                    style: GoogleFonts.spaceGrotesk(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w400,
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      Positioned(
-        left: 139,
-        top: 321,
-        child: Container(
-          padding: const EdgeInsets.all(10),
-          decoration: ShapeDecoration(
-            color: Color(0xFF3391CC),
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-            shadows: [
-              BoxShadow(
-                color: Color(0x3F000000),
-                blurRadius: 4,
-                offset: Offset(0, 4),
-                spreadRadius: 0,
-              )
-            ],
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Firm  ',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 30,
-                  fontFamily: 'Space Grotesk',
-                  fontWeight: FontWeight.w400,
-                  height: 0,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      Positioned(
-        left: 441.87,
-        top: 789.79,
-        child: Transform(
-          transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(3.05),
-          child: Container(
-            width: 421.53,
-            height: 364.29,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage("https://via.placeholder.com/422x364"),
-                fit: BoxFit.fill,
               ),
             ),
           ),
-        ),
-      ),
-      Positioned(
-        left: 59.52,
-        top: 190,
-        child: Transform(
-          transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(0.50),
-          child: Container(
-            width: 137.43,
-            height: 140.33,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage("https://via.placeholder.com/137x140"),
-                fit: BoxFit.cover,
+          Positioned(
+            left: width * 0.4,
+            top: 200,
+            child: Builder(
+              builder: (context) => ElevatedButton(
+                onPressed: () {
+                  //cedar tree
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Color(0xFF3391CC),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                child: Center(
+                  child: Text(
+                    'Firm',
+                    style: GoogleFonts.spaceGrotesk(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
-        ),
+          Positioned(
+            left: width * 0.4,
+            top: height * 0.6,
+            child: Transform(
+              transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(3.05),
+              child: Container(
+                width: 421.53,
+                height: 364.29,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage("https://via.placeholder.com/422x364"),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: width * 0.4,
+            top: height * 0.8,
+            child: Transform(
+              transform: Matrix4.identity()..translate(0.0, 0.0)..rotateZ(0.50),
+              child: Container(
+                width: 137.43,
+                height: 140.33,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage("https://via.placeholder.com/137x140"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-    ],
-  ),
-)
-            ],
-                              ),
-                            ),
-                          ),
     );
   }
 }
