9694afc45888c2a8dc37c8d52d40632020001b47
ChrisShobe <chrisshobe2000@gmail.com>
2024-02-17 20:39:11 -0800

    ishika code: blue or green dots and text fixed

--- flutter_application_1/lib/blueOrGreen.dart
+++ flutter_application_1/lib/blueOrGreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'scaleNeedles.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class BlueORGreenPage extends StatelessWidget {
   @override
@@ -43,10 +45,9 @@ class BlueORGreenPage extends StatelessWidget {
                   child: Text(
                     'Does it have blue and green berries?',
                     textAlign: TextAlign.center,
-                    style: TextStyle(
+                    style: GoogleFonts.spaceGrotesk(
                       color: const Color(0xFFBC3124),
                       fontSize: 30,
-                      fontFamily: 'Space Grotesk',
                       fontWeight: FontWeight.w400,
                       height: 0,
                     ),
@@ -56,6 +57,8 @@ class BlueORGreenPage extends StatelessWidget {
               Positioned(
                 left: 150,
                 top: 416,
+                height: 50,
+                width: 90,
                 child: ElevatedButton(
                   onPressed: () {
                     Navigator.push(
@@ -73,10 +76,10 @@ class BlueORGreenPage extends StatelessWidget {
                   ),
                   child: const Text(
                     'No',
-                    style: TextStyle(
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.spaceGrotesk(
                       color: Colors.black,
-                      fontSize: 30,
-                      fontFamily: 'Space Grotesk',
+                      fontSize: 25,
                       fontWeight: FontWeight.w400,
                       height: 0,
                     ),
@@ -86,6 +89,8 @@ class BlueORGreenPage extends StatelessWidget {
               Positioned(
                 left: 145,
                 top: 281,
+                height: 50,
+                width: 90,
                 child: ElevatedButton(
                   onPressed: () {
                     // Add your logic here for 'Yes'
@@ -110,18 +115,236 @@ class BlueORGreenPage extends StatelessWidget {
                   ),
                 ),
               ),
+              // DOTS --------------------------------------------------
               Positioned(
-                left: 254,
-                top: 634,
+                left: 52,
+                top: 64,
                 child: Container(
-                  width: 37,
-                  height: 35,
+                  width: 18,
+                  height: 19,
                   decoration: const ShapeDecoration(
                     color: Color(0xFF00750B),
                     shape: CircleBorder(),
                   ),
                 ),
               ),
+
+              Positioned(
+                left: 33, // Adjust left position as needed
+                top: 295,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF639F32),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 230, // Adjust left position as needed
+                top: 204,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF639F32),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 34, // Adjust left position as needed
+                top: 600,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF639F32),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 471, // Adjust left position as needed
+                top: 46,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF639F32),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 544, // Adjust left position as needed
+                top: 263,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF639F32),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 270, // Adjust left position as needed
+                top: 500,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF639F32),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 634, // Adjust left position as needed
+                top: 100,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF3391CC),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 733, // Adjust left position as needed
+                top: 322,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF3391CC),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 96, // Adjust left position as needed
+                top: 480,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF3391CC),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 277, // Adjust left position as needed
+                top: 82,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF3391CC),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 258, // Adjust left position as needed
+                top: 313,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF3391CC),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 429, // Adjust left position as needed
+                top: 206,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF3391CC),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 508, // Adjust left position as needed
+                top: 136,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF3391CC),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 698, // Adjust left position as needed
+                top: 224,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF3391CC),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 733, // Adjust left position as needed
+                top: 64,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF3391CC),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+              Positioned(
+                left: 200, // Adjust left position as needed
+                top: 580,  // Adjust top position as needed
+                child: Container(
+                  width: 18,
+                  height: 19,
+                  decoration: ShapeDecoration(
+                    color: Color(0xFF3391CC),
+                    shape: CircleBorder(), // Use CircleBorder to make it oval
+                  ),
+                ),
+              ),
+
+
+
+
+
+
+
+
+
             ],
           ),
         ),
